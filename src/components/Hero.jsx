2fddd689902ccd679ded8f2cb7e7bfd796bc3ba7
MichelPianaro <teconhecer27@gmail.com>
2025-10-28 00:00:10 -0300

    atualização

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import "../App.css";
 import heroImg from "../assets/hero.jpg";
 import profileImg from "../assets/profile.png";
 import project1 from "../assets/projetoluke.png";
-import project2 from "../assets/Projetoitau.png";
+import project2 from "../assets/projetoitau.png";
 import project3 from "../assets/projetosumup.png";
 import project4 from "../assets/projetomkfood.png";
 import florImg from "../assets/flor2.jpg";
